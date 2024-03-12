a5277e36b44da6b1c59233213b6dfe19e60af700
Auto Logon <autologon@CSED.LAB>
2024-03-12 20:20:35 -0300

    Primeiro commit

---
+++ Exercicio01.java
@@ -0,0 +1,19 @@
+public class Exercicio01 {
+    public static void executar() {
+
+        // 1.Escreva um algoritmo que leia um número digitado pelo usuário e mostre
+        // amensagem
+        // “Número maior do que 10!”, caso este número seja maior,
+        // ou “Númeromenor ou igual a 10!”, caso este número seja menor ou igual.
+
+        int num = Prompt.lerInteiro("Digite o número:");
+
+        if (num > 10) {
+            System.out.println("Número maior do que 10!");
+        } else {
+            System.out.println("Número menor ou igual a 10!");
+        }
+
+    }
+
+}
