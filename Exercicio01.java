1246f22d568cfcd26b7a0cab4829f9bc51877484
Gabriela Rodrigues Schorr <gabibusuda123@gmail.com>
2024-03-12 20:46:23 -0300

    Exercicio02

--- Exercicio01.java
+++ Exercicio01.java
@@ -11,7 +11,7 @@ public class Exercicio01 {
         if (num > 10) {
             System.out.println("Número maior do que 10!");
         } else {
-            System.out.println("Número menor ou igual a 10");
+            System.out.println("Número menor ou igual a 10!");
         }
 
     }
---
+++ Exercicio02.java
@@ -0,0 +1,17 @@
+public class Exercicio02{
+        // 2. Escreva um algoritmo que leia dois números digitados pelo usuário
+        // e exiba oresultado da sua soma
+
+        public static void executar() {
+
+            int num1 = Prompt.lerInteiro("Digite o primeiro número:");
+
+            int num2 = Prompt.lerInteiro("Digite o segundo número:");
+
+            System.out.println("A soma é :" + (num1 + num2));
+
+        }
+
+}
+
+
